10cd7bf7a1a6c3b293d2603fabe7eeaea72671a4
Sander-Kastelein <sander@sanderkastelein.nl>
2015-04-15 16:02:12 +0200

    New features for auto HTML extract and reporting

--- index.js
+++ index.js
@@ -16,11 +16,17 @@ var apiInUrl = 'http://2captcha.com/in.php';
 var apiLookupUrl = 'http://2captcha.com/res.php?action=get';
 var apiMethod = 'base64';
 
+var defaultOptions = {
+    pollingInterval: 2000,
+    retries: 3
+};
 
-function pollCaptcha(captchaId, pollingInterval, callback) {
+
+function pollCaptcha(captchaId, options, invalid, callback) {
+    invalid = invalid.bind({options:options,captchaId:captchaId});
     var intervalId = setInterval(function() {
-        var options = url.parse(apiLookupUrl + '&key=' + apiKey + '&id=' + captchaId);
-        var request = http.request(options, function(response) {
+        var httpRequestOptions = url.parse(apiLookupUrl + '&key=' + apiKey + '&id=' + captchaId);
+        var request = http.request(httpRequestOptions, function(response) {
             var body = '';
 
             response.on('data', function(chunk) {
@@ -39,23 +45,27 @@ function pollCaptcha(captchaId, pollingInterval, callback) {
                     callback(null, {
                         id: captchaId,
                         text: result[1]
-                    });
+                    }, invalid);
                 }
                 callback = function() {}; // prevent the callback from being called more than once, if multiple http requests are open at the same time.
             });
         });
 
         request.end();
-    }, pollingInterval);
+    }, (options.pollingInterval || defaultOptions.pollingInterval));
 }
 
 module.exports.setApiKey = function(key) {
     apiKey = key;
 };
 
-module.exports.decode = function(base64, pollingInterval, callback) {
-    var options = url.parse(apiInUrl);
-    options.method = 'POST';
+module.exports.decode = function(base64, options, callback) {
+    if(!callback){
+        callback = options;
+        options = defaultOptions;
+    }
+    var httpRequestOptions = url.parse(apiInUrl);
+    httpRequestOptions.method = 'POST';
 
     var postData = {
         method: apiMethod,
@@ -65,7 +75,7 @@ module.exports.decode = function(base64, pollingInterval, callback) {
 
     postData = querystring.stringify(postData);
 
-    var request = http.request(options, function(response) {
+    var request = http.request(httpRequestOptions, function(response) {
         var body = '';
 
         response.on('data', function(chunk) {
@@ -76,14 +86,28 @@ module.exports.decode = function(base64, pollingInterval, callback) {
             var result = body.split('|');
             if (result[0] !== 'OK')
                 callback(result[0]);
-            pollCaptcha(result[1], pollingInterval, callback);
+            pollCaptcha(result[1], options, function(){
+                module.exports.report(this.captchaId);
+                if(!this.options.retries)
+                    this.options.retries = defaultOptions.retries;
+                if(this.options.retries > 1){
+                    this.options.retries = this.options.retries - 1;
+                    module.exports.decode(base64, this.options, callback);
+                }else{
+                    callback('Failed too many times');
+                }
+            }, callback);
         });
     });
     request.write(postData)
     request.end();
 };
 
-module.exports.decodeUrl = function(uri, pollingInterval, callback) {
+module.exports.decodeUrl = function(uri, options, callback) {
+    if(!callback){
+        callback = options;
+        options = defaultOptions;
+    }
     var protocol = http;
     if (uri.indexOf('https') == 0)
         protocol = https;
@@ -99,8 +123,51 @@ module.exports.decodeUrl = function(uri, pollingInterval, callback) {
         });
 
         response.on('end', function() {
-            module.exports.decode(body, pollingInterval, callback);
+            module.exports.decode(body, options, callback);
         });
     });
     request.end();
+};
+
+module.exports.solveRecaptchaFromHtml = function(html, options, callback){
+    if(!callback){
+        callback = options;
+        options = defaultOptions;
+    }
+    var googleUrl = html.split('/challenge?k=');
+    if(googleUrl.length < 2)
+        return callback('No captcha found in html');
+    googleUrl = googleUrl[1];
+    googleUrl = googleUrl.split('"')[0];
+    googleUrl = googleUrl.split("'")[0];
+    googleUrl = 'https://www.google.com/recaptcha/api/challenge?k='+googleUrl;
+
+    var protocol = http;
+    if (googleUrl.indexOf('https') == 0)
+        protocol = https;
+
+    var httpRequestOptions = url.parse(googleUrl);
+
+    var request = protocol.request(httpRequestOptions, function(response) {
+        var body = '';
+        response.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        response.on('end', function() {
+            var challenge = body.split("'");
+            if (!challenge[1])
+                return callback('Parsing captcha failed');
+            challenge = challenge[1];
+            if (challenge.length === 0)
+                return callback('Parsing captcha failed');
+
+            module.exports.decodeUrl('https://www.google.com/recaptcha/api/image?c='+challenge,options,callback);
+        });
+    });
+    request.end();
+};
+
+module.exports.report = function(captchaId){
+
 };
